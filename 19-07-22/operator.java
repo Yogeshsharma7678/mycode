b35e046ac32bc75f3f71816c99d7f4c06b49d9ac
Yogeshsharma7678 <yogeshmaithil7678@gmail.com>
2022-07-22 08:19:37 -0700

    My First Commit

---
+++ 19-07-22/operator.java
@@ -0,0 +1,13 @@
+class Conditional {
+    
+     public static void main(String args[]){
+        
+     byte a=10, b=13, c=9, d=15;
+     
+     byte X=(a>b)?(a>c?a:c):(b>c?b:c);
+      
+     byte Y=(a>b && b>c)?(b>d?b:d):(a>c?a:c):(c>d?c:d);
+     
+     System.out.println(X);
+   }
+}
---
+++ 21-07-22/series.java
@@ -0,0 +1,15 @@
+class Series {
+  public static void main(String[]args){
+   
+  for(int i=1;i<=121;i++){
+    
+   if(i==0){
+   System.out.println(i);
+   }
+   else{
+   i=i+10;
+   System.out.println(i);
+   }
+  }
+ } 
+}
